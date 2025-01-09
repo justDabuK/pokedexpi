92b9b4af1e5a0fac583cf9ac6e528c6c71c9e6c4
justDabuK <kevinjust87@gmail.com>
2025-01-09 16:24:58 +0100

    feat: add simple stream showing website

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>My own image preview</h1>
+<img src="http://192.168.178.49:8000/stream.mjpg" alt="supposed video stream" />
